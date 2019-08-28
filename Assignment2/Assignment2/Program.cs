6d0cd7197d1f4d88655f12715060233946d69177
siemens-c2c <joy_mon78@yahoo.co.in>
2019-08-28 15:16:27 +0530

    added assignment solution

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("enter sentence to split: ");
+            var result = new WordFrequency().GetWordFrequency(Console.ReadLine());
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+        }
+    }
+}
---
+++ Assignment2/Assignment2/WordFrequency.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class WordFrequency
+    {
+        public static char[] GetSeparators(string sentence)
+        {
+            HashSet<char> separators = new HashSet<char>();
+            foreach (char item in sentence)
+            {
+                if (!char.IsLetter(item) && !char.IsDigit(item))
+                {
+                    separators.Add(item);
+                }
+            }
+            return separators.ToArray();
+        }
+        public SortedList<string, int> GetWordFrequency(string sentence)
+        {
+            SortedList<string, int> result = null;
+            if (sentence != null && sentence != string.Empty)
+            {
+                result = new SortedList<string, int>();
+
+                var separators = GetSeparators(sentence);
+                string[] words = sentence.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string word in words)
+                {
+                    if (result.ContainsKey(word.ToLower()))
+                    {
+                        result[word.ToLower()] += 1;
+                    }
+                    else
+                        result.Add(word.ToLower(), 1);
+                }
+            }
+            return result;
+        }
+    }
+}
